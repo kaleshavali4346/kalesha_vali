bb4dcb3d75a0223145aa02d3b0a3ca7f58f95461
kaleshavali4346 <shaikkalesha.vali2021@vitstudent.ac.in>
2024-09-06 23:07:46 +0530

    Added contact components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <About/>
         <Technologies/>
         <Experience/>
+        <Contact/>
       </div>
     </div>
   );}
