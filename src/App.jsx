f50cc9bfe3d2f221e9d8bfdbe406d9ff81aeb0d0
kaleshavali4346 <shaikkalesha.vali2021@vitstudent.ac.in>
2024-09-06 22:20:19 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
-import Project from "./components/Projects";
 
 function App() {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-neutral-400 selection:text-neutral-400">
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       </div>
@@ -18,7 +17,6 @@ function App() {
         <About/>
         <Technologies/>
         <Experience/>
-        <Project/>
       </div>
     </div>
   );}
